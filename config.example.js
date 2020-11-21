b36e589c6868b3e18b61900036e615d773018e2a
Claire Melbourne <claire.melbourne@gmail.com>
2020-11-21 13:59:35 -0600

    add config example file

---
+++ config.example.js
@@ -0,0 +1,8 @@
+//TO DO: You will need your own API keys from Unsplash and Google Cloud Products. Rename or copy this file as config.js with your respective tokens filled in for Unsplash and GCP.
+// https://unsplash.com/developers
+// https://cloud.google.com/docs/authentication/api-keys
+
+module.exports = {
+  UNSPLASH_TOKEN : '',
+  GCP_TOKEN : ''
+}
